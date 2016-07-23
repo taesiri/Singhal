6de36e54f8379ee88e80c41d33fd48003a32b16e
taesiri <taesiri@live.com>
2016-07-23 19:25:43 +0430

    Benchmark 9 Node

--- jobGenerator/singleResource.js
+++ jobGenerator/singleResource.js
@@ -1,8 +1,8 @@
 
-for (var i = 1; i<10; i++) {
+for (var i = 1; i<250; i++) {
   var nodeId = Math.round(getRandom(1,3));
   var jobTime = Math.round(getRandom(1,100)) * 10;
-  var resources = 1; // Math.round(getRandom(0,4));
+  var resources = Math.round(getRandom(0,4));
 
   console.log(i + ":" + nodeId + ":" + jobTime + ":" + resources );
 }
